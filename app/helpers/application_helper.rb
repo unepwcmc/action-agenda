1729a9dfdfc4aa6fb21a39771fe6058a0076d9b3
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2020-12-01 16:19:20 +0000

    More front end work on the homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def footer_nav
+    navPresenter = NavigationPresenter.new
+    
+    navPresenter.nav_footer
+  end
+
+  def footer_icons
+  end
 end
---
+++ app/presenters/navigation_presenter.rb
@@ -0,0 +1,26 @@
+class NavigationPresenter
+
+  def initialize
+  end
+
+  def nav_footer 
+    [
+      {
+        title: 'Make a Commitment',
+        url: ''
+      },
+      {
+        title: 'Explore Commitments',
+        url: ''
+      },
+      {
+        title: 'About',
+        url: ''
+      },
+      {
+        title: 'Main Action Agenda',
+        url: ''
+      }
+    ]
+  end
+end
