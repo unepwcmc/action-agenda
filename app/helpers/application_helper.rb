94b84e56723d7e046280ce711969e237ebd5768e
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2020-12-08 10:55:17 +0000

    Update navigations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def header_nav
+    navPresenter = NavigationPresenter.new
+    
+    navPresenter.nav_header
+  end
+
   def footer_nav
     navPresenter = NavigationPresenter.new
     
--- app/presenters/navigation_presenter.rb
+++ app/presenters/navigation_presenter.rb
@@ -1,25 +1,47 @@
 class NavigationPresenter
+  include Rails.application.routes.url_helpers
 
   def initialize
+  end
+
+  def nav_header
+    [
+      {
+        title: 'Home',
+        url: '/'
+      },
+      {
+        title: 'Make a Commitment',
+        url: '#'
+      },
+      {
+        title: 'Explore Commitments',
+        url: commitments_path
+      },
+      {
+        title: 'About',
+        url: '#'
+      }
+    ]
   end
 
   def nav_footer 
     [
       {
         title: 'Make a Commitment',
-        url: ''
+        url: '#'
       },
       {
         title: 'Explore Commitments',
-        url: ''
+        url: commitments_path
       },
       {
         title: 'About',
-        url: ''
+        url: '#'
       },
       {
         title: 'Main Action Agenda',
-        url: ''
+        url: 'https://www.cbd.int/action-agenda/'
       }
     ]
   end
