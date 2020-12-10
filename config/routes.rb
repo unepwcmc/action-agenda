ebd8f99c07c78f49fe41b597a405405d42c523f5
Ben Tregenna <ben.tregenna@unep-wcmc.org>
2020-12-10 10:22:48 +0000

    Started tests and made neat 404 for missing commitment

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/', to: 'home#index'
 
-  get '/commitments', to: 'commitments#index'
+  get '/commitments', to: 'commitments#index', as: 'commitments'
 
   get '/commitments/:id', to: 'commitments#show', as: 'commitment'
   post '/commitments/list', to: 'commitments#list'
