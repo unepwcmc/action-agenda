876e9b274715594f63373bf64d2d7e06e7e813c7
sergio <s.marrocoli@gmail.com>
2022-01-14 14:52:12 +0000

    Added failure messages to criterium

--- app/models/criterium.rb
+++ app/models/criterium.rb
@@ -5,11 +5,14 @@ class Criterium < ApplicationRecord
   validates_inclusion_of :boundary, :five_year_commitment, :progress_report, in: [true, false], message: :nil_value
 
   def criteria_valid_for_commitment?
-    ![boundary, five_year_commitment, progress_report, cbd_objective_ids, stakeholder_ids].map(&:blank?).include?(true)
+    ![boundary, five_year_commitment, progress_report, cbd_objectives, stakeholders].map(&:blank?).include?(true)
   end
 
   def failure_messages
     messages = []
-    
+    %w(boundary five_year_commitment progress_report cbd_objectives stakeholders).each do |attribute|
+      messages << I18n.t(:"criterium.failure_messages.#{attribute.to_s}") if self.send(attribute).blank?
+    end
+    messages
   end
 end
