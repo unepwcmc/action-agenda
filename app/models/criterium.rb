49e4e830a01fccc2cf45560d41be1f129180ba28
sergio <s.marrocoli@gmail.com>
2022-01-14 13:28:01 +0000

    Removed None of theabove optiosn to simplify

--- app/models/criterium.rb
+++ app/models/criterium.rb
@@ -3,36 +3,8 @@ class Criterium < ApplicationRecord
   has_and_belongs_to_many :stakeholders
 
   validates_inclusion_of :boundary, :five_year_commitment, :progress_report, in: [true, false], message: :nil_value
-  validates :cbd_objectives, presence: true
-  validates :stakeholders, presence: true
-  validate :none_of_the_above_for_cbd_commitments
-  validate :none_of_the_above_for_stakeholders
 
   def criteria_valid_for_commitment?
-    !has_failing_data
-  end
-
-  private
-
-  def none_of_the_above_for_cbd_commitments
-    if includes_none_of_the_above(CbdObjective, cbd_objective_ids) && cbd_objectives.length > 1
-      errors.add(:base, :none_of_the_above_option_for_cbd_commitments)
-    end
-  end
-  
-  def none_of_the_above_for_stakeholders
-    if includes_none_of_the_above(Stakeholder, stakeholder_ids) && stakeholders.length > 1
-      errors.add(:base, :none_of_the_above_option_for_stakeholders)
-    end
-  end
-
-  def includes_none_of_the_above(klass, record_ids) 
-    klass.where(id: record_ids).pluck(:name).include?('None of the above')
-  end
-
-  def has_failing_data
-    [boundary, five_year_commitment, progress_report].map(&:blank?).include?(true) ||
-        includes_none_of_the_above(CbdObjective, cbd_objective_ids) ||
-          includes_none_of_the_above(Stakeholder, stakeholder_ids)
+    ![boundary, five_year_commitment, progress_report, cbd_objective_ids, stakeholder_ids].map(&:blank?).include?(true)
   end
 end
--- test/models/criterium_test.rb
+++ test/models/criterium_test.rb
@@ -4,7 +4,7 @@ class CriteriumTest < ActiveSupport::TestCase
   test "should not save criteria with missing attributes" do
     criterium = Criterium.new
     assert_not criterium.save
-    assert criterium.errors.count == 5
+    assert criterium.errors.count == 3
   end
 
   test "should be invalid for commitment if it has negative values" do
@@ -14,12 +14,5 @@ class CriteriumTest < ActiveSupport::TestCase
     assert_not criteria(:no_progress_report).criteria_valid_for_commitment?
     assert_not criteria(:no_cbd_objectives).criteria_valid_for_commitment?
     assert_not criteria(:no_stakeholders).criteria_valid_for_commitment?
-    assert_not criteria(:invalid_cbd_objectives_and_stakeholders).criteria_valid_for_commitment?
-  end
-
-  test "should not save if has both 'None of the above' and other options" do
-    invalid_criterium = criteria(:invalid_cbd_objectives_and_stakeholders)
-    assert_not invalid_criterium.save
-    assert invalid_criterium.errors.count == 2
   end
 end
