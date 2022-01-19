6131eafabd8709bb6c3d9d00f9f00a0c40b028e8
Dagmara <dagmarakukla@gmail.com>
2022-01-19 19:46:41 +0000

    styles: add white gap to the progress bar

--- app/javascript/components/progress/MockProgressBar.vue
+++ app/javascript/components/progress/MockProgressBar.vue
@@ -2,7 +2,8 @@
   <div class="container">
     <ul class="progress">
       <li class="progress__active">{{ title.active }}</li>
-      <li class="progress__dormant">{{ title.dormant }}</li>
+      <!-- the span has a peudoelement attached for styling -->
+      <li class="progress__dormant"><span></span>{{ title.dormant }}</li>
     </ul>
   </div>
 </template>
