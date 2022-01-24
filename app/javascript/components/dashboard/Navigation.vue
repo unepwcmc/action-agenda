c5fc71d3ae7afde46499d3373d163e77bae2fdc4
Ynda Jas <yndajas@gmail.com>
2022-01-24 17:59:15 +0000

    feat: dashboard navigation

--- app/javascript/components/dashboard/Navigation.vue
+++ app/javascript/components/dashboard/Navigation.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="dashboard-navigation">
-    Dashboard navigation
-  </div>
+  <ul class="dashboard-navigation">
+    <li
+      v-for="itemKey in Object.keys(navigation)"
+      class="dashboard-navigation__item"
+    >
+      <a
+        :href="navigation[itemKey].path"
+        class="dashboard-navigation__item-link"
+        v-text="navigation[itemKey].text"
+      />
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
-  name: "DashboardNavigation"
+  name: "DashboardNavigation",
+
+  props: {
+    navigation: {
+      type: Object,
+      required: true
+    }
+  }
 }
 </script>
