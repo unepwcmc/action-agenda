2691c3c026ce1c14cf29a15aae976baae6d54620
Ynda Jas <yndajas@gmail.com>
2022-01-25 13:07:31 +0000

    fix: update user controller comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def dashboard
-    commitments = Commitment.limit(5)
+    commitments = Commitment.limit(5) # placeholder until we have users in place
     @commitments = commitments.to_json
-    # for some reason .empty? returns true here if using @commitments with an empty array
     @has_commitments = !commitments.empty?
   end
 end
