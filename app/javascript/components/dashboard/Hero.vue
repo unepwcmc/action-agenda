d2d7936b11b27574be6bc6ba461738773676dd75
Ynda Jas <yndajas@gmail.com>
2022-01-25 13:11:37 +0000

    fix: quote marks

--- app/javascript/components/dashboard/Hero.vue
+++ app/javascript/components/dashboard/Hero.vue
@@ -4,7 +4,7 @@
     :class="modifierClass"
   >
     <div 
-      :class="{ 'container': !hasCommitments }"
+      :class="{ "container": !hasCommitments }"
     >
       <h2
         class="dashboard-hero__title"
