48c00242cb8f36c54c5df9bfa228caaf52dfb7c7
Ynda Jas <yndajas@gmail.com>
2022-01-25 16:37:45 +0000

    refactor: table child component; missing button; present?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def dashboard
     commitments = Commitment.limit(5) # placeholder until we have users in place
     @commitments = commitments.to_json
-    @has_commitments = !commitments.empty?
+    @has_commitments = commitments.present?
   end
 end
