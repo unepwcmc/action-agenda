79aa88b97c8020bb5b8d5ca4717c7de1fda6a79b
Ynda Jas <yndajas@gmail.com>
2022-01-25 17:28:39 +0000

    feat: destroy commitments route and action

--- app/controllers/commitments_controller.rb
+++ app/controllers/commitments_controller.rb
@@ -6,7 +6,7 @@ class CommitmentsController < ApplicationController
     filters: []
   }.to_json
 
-  before_action :set_commitment, only: [:show, :edit, :update]
+  before_action :set_commitment, only: [:show, :edit, :update, :destroy]
 
   def index
     @paginatedCommitments = Commitment.paginate_commitments(DEFAULT_PARAMS).to_json
@@ -63,6 +63,18 @@ class CommitmentsController < ApplicationController
     if @commitment.update(commitment_params)
       respond_to do |format|
         format.json { json_response({ commitment: @commitment }, 204) }
+      end
+    else
+      respond_to do |format|
+        format.json { json_response({ errors: @commitment.errors }, :unprocessable_entity) }
+      end
+    end
+  end
+
+  def destroy
+    if @commitment.destroy
+      respond_to do |format|
+        format.json { json_response({}, 204) }
       end
     else
       respond_to do |format|
