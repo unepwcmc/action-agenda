e34ec90da5972c78cfa6b82d9012df965888715f
sergio <s.marrocoli@gmail.com>
2022-01-26 13:49:04 +0000

    Removed letter opener

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,8 +64,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "action-agenda_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
