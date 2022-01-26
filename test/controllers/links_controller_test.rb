990f105741828f54be38180342db49e6a73f5c28
sergio <s.marrocoli@gmail.com>
2022-01-26 09:58:41 +0000

    Added devise test for unauthroized attempts to destroy and update links

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class LinksControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   
   test "should update a link" do
+    sign_in users(:user_1)
     update_params = { link: { name: "a new name", url: "new.site.com" }}
     link = links(:link_1)
     assert link.name != update_params.dig(:link, :name)
@@ -15,10 +17,25 @@ class LinksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy a link" do
+    sign_in users(:user_1)
     link = links(:link_1)
     link_count = Link.count
     delete link_url(link), as: :json
     assert_response 204
     assert Link.count == link_count - 1
+  end
+
+  test "PUT update should redirect to sign in unless signed in" do
+    link = links(:link_1)
+    put link_url(link), as: :json
+    assert_response :unauthorized
+    assert JSON.parse(response.body).dig('error') == I18n.t('devise.failure.unauthenticated')
+  end
+
+  test "DELETE destroy should redirect to sign in unless signed in" do
+    link = links(:link_1)
+    delete link_url(link), as: :json
+    assert_response :unauthorized
+    assert JSON.parse(response.body).dig('error') == I18n.t('devise.failure.unauthenticated')
   end
 end
