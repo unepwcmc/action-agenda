074149739bbf256bedf37008746b74a46c7db808
Dagmara <dagmarakukla@gmail.com>
2022-01-27 19:24:10 +0000

    feat: prevent form from completing on BE error

--- app/javascript/components/forms/SurveyForm.vue
+++ app/javascript/components/forms/SurveyForm.vue
@@ -6,7 +6,7 @@
         complete,
         dataModel,
         exit,
-        isFirstPage, 
+        isFirstPage,
         isLastPage,
         modalQuestionBody,
         modalText,
@@ -15,30 +15,30 @@
         nextPage,
       }"
     />
-    <error-banner :errors="errors"/>
+    <error-banner :errors="errors" />
   </div>
 </template>
 
 <script>
-import * as SurveyVue from "survey-vue";
-import * as widgets from "surveyjs-widgets";
-import Turbolinks from "turbolinks";
-import axios from "axios";
-import { setAxiosHeaders } from "../../helpers/axios-helpers";
-import "survey-vue/modern.css";
-import FormNavigation from "./Navigation"
-import ErrorBanner from '../banners/ErrorBanner'
+import * as SurveyVue from 'survey-vue';
+import * as widgets from 'surveyjs-widgets';
+import Turbolinks from 'turbolinks';
+import axios from 'axios';
+import { setAxiosHeaders } from '../../helpers/axios-helpers';
+import 'survey-vue/modern.css';
+import FormNavigation from './Navigation';
+import ErrorBanner from '../banners/ErrorBanner';
 
-SurveyVue.StylesManager.applyTheme("modern");
+SurveyVue.StylesManager.applyTheme('modern');
 
 widgets.select2tagbox(SurveyVue);
 
 const Survey = SurveyVue.Survey;
 
 export default {
-  name: "SurveyForm",
+  name: 'SurveyForm',
 
-  components: { 
+  components: {
     ErrorBanner,
     FormNavigation,
     Survey,
@@ -54,7 +54,7 @@ export default {
       type: String,
       // required: true
     },
-  
+
     modalText: {
       type: Object,
       // required: true
@@ -72,111 +72,131 @@ export default {
 
     noneValues: {
       type: Object,
-      default: () => ({})
-    }
+      default: () => ({}),
+    },
   },
 
-  data () {
+  data() {
     const model = new SurveyVue.Model(this.formData.survey);
     // call methods on library-provided events here
     model.onComplete.add(this.onComplete);
     model.onCurrentPageChanged.add(this.onCurrentPageChanged);
+    model.onCompleting.add(this.onCompleting);
 
     return {
       errors: {},
       isFirstPage: true,
       isLastPage: false,
       options: {},
-      survey: model
+      survey: model,
     };
+  },
+  computed: {
+    hasErrors() {
+      return Object.keys(this.errors).length > 0
+    }
   },
 
   mounted() {
     setAxiosHeaders(axios);
-    console.log(this.formData)
+    console.log(this.formData);
   },
 
   methods: {
     assignNoneValues(data) {
       Object.keys(this.noneValues).forEach((question) => {
-        console.log(data[question])
-        if (data[question] && data[question][0] === "none") {
-          console.log(data[question][0])
-          console.log(this.noneValues[question])
-          data[question][0] = this.noneValues[question]
+        console.log(data[question]);
+        if (data[question] && data[question][0] === 'none') {
+          console.log(data[question][0]);
+          console.log(this.noneValues[question]);
+          data[question][0] = this.noneValues[question];
         }
       });
     },
 
     axiosCall() {
       axios(this.formData.config.action, this.options)
-       .then((response) => {
-          this.redirect(response.data.redirect_path)
+        .then((response) => {
+          this.errors = {}
+          this.redirect(response.data.redirect_path);
         })
         .catch((error) => {
-          console.log("FAILED!", error.response.data.errors)
-                    this.survey.options.allowComplete = false
-          this.errors = error.response.data.errors
-          // this.survey.options.allowComplete = false
-        })
+          console.log('FAILED!', error.response.data.errors);
+          this.errors = error.response.data.errors;
+        });
     },
 
-    complete () {
-      this.survey.completeLastPage()
+    complete() {
+      this.survey.completeLastPage();
     },
 
-    exit () {
-      if (this.dataModel === "Commitment") {
-        this.survey.completeLastPage()
+    exit() {
+      if (this.dataModel === 'Commitment') {
+        this.survey.completeLastPage();
       } else {
-        Turbolinks.visit("/dashboard")
+        this.redirect('/dashboard');
       }
     },
 
-    nextPage () {
-      this.survey.nextPage()
+    nextPage() {
+      this.survey.nextPage();
     },
 
     onComplete(sender) {
-      const data = sender.data;
-      if (this.dataModel === "Commitment") {
-        data["state"] = "live"
+      const data = sender.data
+      if (this.dataModel === 'Commitment') {
+        data['state'] = 'live';
       } else {
         this.assignNoneValues(data);
       }
       this.options = {
         method: this.formData.config.method,
         data: { [this.formData.config.root_key]: data },
-      }
-
-      this.axiosCall()
+      };
+      this.axiosCall();
     },
 
-    onCurrentPageChanged () {
+    async onCompleting(sender, options) {
+      if (this.dataModel === 'Commitment') {
+        let data = sender.data;
+        data['state'] = 'live';
+        this.options = {
+          method: this.formData.config.method,
+          data: { [this.formData.config.root_key]: data },
+        };
+        await this.axiosCall();
+        console.log("errors", this.errors)
+        options.allowComplete = this.hasErrors
+      } else {
+        options.allowComplete = true;
+      }
+    },
+
+    onCurrentPageChanged() {
       this.isFirstPage = this.survey.isFirstPage;
       this.isLastPage = this.survey.isLastPage;
 
-      if (this.dataModel === "Commitment") {
-        const data = this.survey.data
+      if (this.dataModel === 'Commitment') {
+        const data = this.survey.data;
 
         this.options = {
           method: this.formData.config.method,
           data: { [this.formData.config.root_key]: data },
-        }
-        this.axiosCall()
+        };
+        this.axiosCall();
         //
       }
     },
-  
-    prevPage () {
+
+    prevPage() {
       this.survey.prevPage();
     },
 
     redirect(link) {
       if (link) {
-        Turbolinks.visit(link)
+        Turbolinks.visit(link);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
