e01a906890f7c71ab6e9bd5b8df15d9846744a6c
Dagmara <dagmarakukla@gmail.com>
2022-01-28 10:16:17 +0000

    bug: caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,10 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
+  
+  # Do not compress assets
+  config.assets.compress = false
+  
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
@@ -53,6 +56,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
