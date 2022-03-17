f3af6a4f777c9fba3417633cc718938686a3cf9e
sergio <s.marrocoli@gmail.com>
2022-03-17 17:30:54 +0000

    Add None of the above to original manager options

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,7 +1,7 @@
 class Manager < ApplicationRecord
   DEFAULT_OPTIONS = [
-    'Local communities', 'Indigenous peoples', 'Non-governmental organisation (NGO)', 
-    'Private sector (business and industry)', 'Sub-national or local government', 'None of the above'
+    'Indigenous peoples', 'Local communities', 'For-profit organisations', 'Non-profit organisations', 'Individual landowners',
+    'Collaborative governance', 'Joint governance', 'Sub-national ministry or agency', 'None of the above'
   ]
 
   scope :commitment_form_options, -> { where("default_option IS true AND name != 'None of the above'") }
