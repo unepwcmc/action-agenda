d12fa767d25b9f688c2c45f80f036d3124c45ce0
Dagmara <dagmarakukla@gmail.com>
2022-03-17 16:32:13 +0000

    styles: flash message

---
+++ app/javascript/components/map/MapPopup.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <p>
+      {{ content.name }},
+      <strong>{{ content.commitment_count }}</strong> commitments
+    </p>
+    <BarChart :chartData="data" :options="options" :key="randomKey" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import BarChart from "../chart/BarChart";
+import { setAxiosHeaders } from "../../helpers/axios-helpers";
+
+export default {
+  name: "MapPopup",
+
+  components: { BarChart },
+
+  props: {
+    content: {
+      type: Object,
+    },
+  },
+
+  data() {
+    return {
+      id: this.content.id,
+      chartData: Object,
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: this.content.name,
+            data: [],
+            backgroundColor: [
+              "rgb(255, 99, 132)",
+              "rgb(255, 159, 64)",
+              "rgb(255, 205, 86)",
+              "rgb(75, 192, 192)",
+              "rgb(54, 162, 235)",
+              "rgb(153, 102, 255)",
+              "rgb(201, 203, 207)",
+              "rgb(255, 99, 132)",
+              "rgb(255, 159, 64)",
+              "rgb(75, 192, 192)",
+              "rgb(54, 162, 235)",
+              "rgb(255, 99, 132)",
+              "rgb(255, 159, 64)",
+              "rgb(75, 192, 192)",
+              "rgb(54, 162, 235)",
+            ],
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                display: false,
+              },
+            },
+          ],
+        },
+        //TODO pass the vars to the legend
+        legend: {
+          display: false,
+          // position: 'right'
+        },
+      },
+      randomKey: Math.random(),
+    };
+  },
+
+  created() {
+    //api call based on the content?
+    setAxiosHeaders(axios);
+    this.$root.$on(`popup:${this.id}`, this.getChartData);
+  },
+
+  methods: {
+    getChartData(id) {
+      const url = `/country_commitments/${id}.json`;
+      // TODO handle error
+      axios
+        .get(url)
+        .then((response) => {
+          this.chartData = response.data.managers;
+          console.log(response.data);
+        })
+        .then(() => this.populateChartData());
+    },
+
+    populateChartData() {
+      //reset the data array so it doesn't push to it on each click
+      this.randomKey += 1;
+      this.data.labels = [];
+      this.data.datasets[0].data = [];
+      this.chartData.forEach((item) => {
+        this.data.labels.push(item.name),
+          this.data.datasets[0].data.push(item.count),
+          console.log(item.count);
+      });
+    },
+  },
+};
+</script>
