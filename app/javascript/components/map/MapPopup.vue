c152d7fb62809791b5df6c6727412736152341c4
Dagmara <dagmarakukla@gmail.com>
2022-03-17 21:33:20 +0000

    fix: scale add dummy button

--- app/javascript/components/map/MapPopup.vue
+++ app/javascript/components/map/MapPopup.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <p>{{ content.name}}, <strong>{{ content.commitment_count }}</strong> commitments</p>
+  <div class="popup__container">
+    <p>
+      {{ content.name }},
+      <strong>{{ content.commitment_count }}</strong> commitments
+    </p>
     <BarChart :chartData="data" :options="options" :key="randomKey" />
+    <button class="map__button">view commitments</button>
   </div>
 </template>
 
@@ -26,70 +30,43 @@ export default {
       id: this.content.id,
       chartData: Object,
       colors: [
-              "rgb(255, 99, 132)",
-              "rgb(255, 159, 64)",
-              "rgb(255, 205, 86)",
-              "rgb(75, 192, 192)",
-              "rgb(54, 162, 235)",
-              "rgb(153, 102, 255)",
-              "rgb(201, 203, 207)",
-              "rgb(255, 99, 132)",
-              "rgb(255, 159, 64)",
-              "rgb(75, 192, 192)",
-              "rgb(54, 162, 235)",
-              "rgb(255, 99, 132)",
-              "rgb(255, 159, 64)",
-              "rgb(75, 192, 192)",
-              "rgb(54, 162, 235)",
-            ],
-          // },
+        "rgb(255, 99, 132)",
+        "rgb(255, 159, 64)",
+        "rgb(255, 205, 86)",
+        "rgb(75, 192, 192)",
+        "rgb(54, 162, 235)",
+        "rgb(153, 102, 255)",
+        "rgb(201, 203, 207)",
+        "rgb(255, 99, 132)",
+        "rgb(255, 159, 64)",
+        "rgb(75, 192, 192)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 99, 132)",
+        "rgb(255, 159, 64)",
+        "rgb(75, 192, 192)",
+        "rgb(54, 162, 235)",
+      ],
       data: {
         labels: [this.content.name],
-        datasets: [
-          // {
-            // label: [],
-            // data: [],
-            // backgroundColor: [
-            //   "rgb(255, 99, 132)",
-            //   "rgb(255, 159, 64)",
-            //   "rgb(255, 205, 86)",
-            //   "rgb(75, 192, 192)",
-            //   "rgb(54, 162, 235)",
-            //   "rgb(153, 102, 255)",
-            //   "rgb(201, 203, 207)",
-            //   "rgb(255, 99, 132)",
-            //   "rgb(255, 159, 64)",
-            //   "rgb(75, 192, 192)",
-            //   "rgb(54, 162, 235)",
-            //   "rgb(255, 99, 132)",
-            //   "rgb(255, 159, 64)",
-            //   "rgb(75, 192, 192)",
-            //   "rgb(54, 162, 235)",
-            // ],
-          // },
-        ],
+        datasets: [],
       },
       options: {
-        // responsive: false,
-        // maxWidth: 120,
+        responsive: false,
         maintainAspectRatio: false,
-        aspectRatio: 1.5,
         scales: {
           yAxes: [
             {
               ticks: {
-                max: 2,
-                min: 0
-                // display: false,
+                min: 0,
+                display: false,
               },
             },
           ],
         },
-        //TODO pass the vars to the legend
         legend: {
           display: true,
-          position: 'bottom',
-          align: 'start',
+          position: "top",
+          align: "start",
           maxWidth: 40,
         },
       },
@@ -119,18 +96,14 @@ export default {
     populateChartData() {
       //reset the data array so it doesn't push to it on each click
       this.randomKey += 1;
-      // this.data.labels = [];
       this.data.datasets = [];
 
-
       this.chartData.forEach((item, key) => {
-        // this.data.labels.push(item.name),
-
-          this.data.datasets.push({ data: [item.count],
+        this.data.datasets.push({
+          data: [item.count],
           label: item.name,
-          backgroundColor: this.colors[key]}),
-          
-          console.log(item.count, this.data.datasets)
+          backgroundColor: this.colors[key],
+        })
       });
     },
   },
