4a5d731c96bb66ecd2e9030f8c3e6d3a82434a79
sergio <s.marrocoli@gmail.com>
2022-04-01 15:24:30 +0100

    Removed Commitment seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,29 +28,3 @@ Country.import
 Action.create!(Action::DEFAULT_OPTIONS.map {|action_name| { name: action_name, default_option: true }})
 Threat.create!(Threat::DEFAULT_OPTIONS.map {|threat_name| { name: threat_name, default_option: true }})
 Objective.create!(Objective::DEFAULT_OPTIONS.map {|objective_name| { name: objective_name, default_option: true }})
-
-commitments = [
-  {
-    geographic_boundary: true, latitude: 0.002, longitude: 0.9, current_area_ha: 12, proposed_area_ha: 15, 
-    committed_year: 2021, implementation_year: 2022, name: 'A commitment', 
-    governance_authority: 'A governance authority name', description: 'A description',
-    duration_years: 5, user: User.first, criterium: Criterium.first,
-    stage: 'Implemented fully', responsible_group: 'The responsible group', state: 'live',
-    threats: Threat.all, actions: Action.all, managers: Manager.all, countries: Country.where(id: 1..5), objectives: Objective.all
-  },
-  {
-    geographic_boundary: true, latitude: 0.002, longitude: 0.9, current_area_ha: 12, proposed_area_ha: 15, 
-    committed_year: 2021, implementation_year: 2022, name: 'A commitment', 
-    governance_authority: 'A governance authority name', description: 'A description',
-    duration_years: 5, user: User.second, criterium: Criterium.first,
-    stage: 'Implemented fully', responsible_group: 'The responsible group', state: 'live',
-    threats: Threat.all, actions: Action.all, managers: Manager.all, countries: Country.where(id: 1..5), objectives: Objective.all
-  }
-]
-Commitment.create!(commitments)
-
-links = [
-  { url: 'something.com', commitment: Commitment.first },
-  { url: 'something.org', commitment: Commitment.first }
-]
-Link.create!(links)
