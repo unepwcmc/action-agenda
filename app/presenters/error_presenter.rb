4c75757cc9dba959908c5c58b5499fd09397bafd
Ynda Jas <yndajas@gmail.com>
2022-04-05 11:50:03 +0100

    fix: casing of fields and messages in errors

--- app/presenters/error_presenter.rb
+++ app/presenters/error_presenter.rb
@@ -1,7 +1,15 @@
 class ErrorPresenter
   def error_messages(resource)
     resource.errors.messages.map do |field, error_messages|
-      error_messages.map { |error_message| "#{field} #{error_message}" }
+      # convert field from snake case
+      recased_field_name = ActiveSupport::Inflector.humanize(field, capitalize: false)
+
+      error_messages.map do |error_message|
+        # make error message casing consistent (not default for Devise)
+        recased_error_message = error_message.downcase
+
+        "#{recased_field_name} #{recased_error_message}"
+      end
     end.flatten
   end
 end
