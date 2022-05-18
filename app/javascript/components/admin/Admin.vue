ed00a14593bc077856326ec96f8f05d17efedc1c
Ynda Jas <yndajas@gmail.com>
2022-05-18 13:19:48 +0100

    refactor: admin components

---
+++ app/javascript/components/admin/Admin.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <admin-tabs v-bind="{ changeTab, currentTab }" />
+    <admin-table v-bind="{ commitments, currentTab, reviewCommitment }" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { setAxiosHeaders } from '../../helpers/axios-helpers';
+import AdminTable from './Table';
+import AdminTabs from './Tabs';
+
+export default {
+  name: 'Admin',
+
+  components: {
+    AdminTable,
+    AdminTabs
+  },
+
+
+  created () {
+    setAxiosHeaders(axios);
+    this.fetchCommitments();
+  },
+
+  data () {
+    return {
+      commitments: [],
+      currentTab: 'Unassigned'
+    };
+  },
+
+  computed: {
+    filter () {
+      switch (this.currentTab) {
+        case 'Unassigned':
+          return null;
+        case 'Approved':
+          return true;
+        case 'Rejected':
+          return false;
+        default:
+          return 'Unassigned'; // null?
+      }
+    }
+  },
+
+  methods: {
+    changeTab(tab) {
+      this.currentTab = tab;
+      this.fetchCommitments();
+    },
+
+    fetchCommitments () {
+      axios
+        .get(
+          'imported_commitments.json',
+          { params: { cfn_approved: this.filter } }
+        )
+        .then((response) => {
+          this.commitments = response.data;
+        })
+    },
+
+    reviewCommitment (value, commitmentId) {
+      axios
+        .put(
+          `imported_commitments/${commitmentId}.json`,
+          { commitment: { cfn_approved: value } }
+        )
+        .then((response) => {
+          this.fetchCommitments();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+}
+</script>
