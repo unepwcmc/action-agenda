ff01566e9d6d498b045424744cce84ad3d27a7fe
sergio <s.marrocoli@gmail.com>
2022-05-18 12:23:00 +0100

    feat: Added spec for admin_imported_commitments controller

--- test/controllers/admin/imported_commitments_controller_test.rb
+++ test/controllers/admin/imported_commitments_controller_test.rb
@@ -5,9 +5,6 @@ require 'test_helper'
 class Admin::ImportedCommitmentsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  # should not get index if not admin
-  # should not put update if not admin
-
   # should get index if admin
   # should put update if admin
 
@@ -19,14 +16,25 @@ class Admin::ImportedCommitmentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should not put update if not admin' do
-  
+    sign_in users(:user_1)
+    commitment = commitments(:rejected_cbd_commitment)
+    put admin_imported_commitment_url(commitment, params: { commitment: { cfn_approved: true } }), as: :json
+    assert_response :found
+    assert_redirected_to root_path
   end
 
   test 'should get index if admin' do
-  
+    sign_in users(:admin_user)
+    get admin_imported_commitments_url
+    assert_response :ok
   end
 
   test 'should put update if admin' do
-  
+    sign_in users(:admin_user)
+    commitment = commitments(:rejected_cbd_commitment)
+    put admin_imported_commitment_url(commitment, params: { commitment: { cfn_approved: true } }), as: :json
+    assert_response :ok
+    commitment.reload
+    assert commitment.cfn_approved == true
   end
 end
