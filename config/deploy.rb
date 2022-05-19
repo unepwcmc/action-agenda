07d171a23f730ab48fdb52da4bf8781d0dae749a
sergio <s.marrocoli@gmail.com>
2022-05-19 21:04:22 +0100

    chore: trying new sidekiq config

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,6 +68,10 @@ set :keep_releases, 5
 set :passenger_restart_with_touch, false
 
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+
+SSHKit.config.command_map[:sidekiq] = "bundle exec sidekiq"
+SSHKit.config.command_map[:sidekiqctl] = "bundle exec sidekiqctl"
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
