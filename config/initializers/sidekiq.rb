2ad6cfc400d3ffff7a556d682532aa18771ecc5a
sergio <s.marrocoli@gmail.com>
2022-05-19 20:47:36 +0100

    chore: removed sidekiq queue prefixes

--- config/initializers/sidekiq.rb
+++
@@ -1,7 +0,0 @@
-Sidekiq.configure_client do |config|
-  config.redis = { namespace: Rails.configuration.active_job.queue_name_prefix }
-end
-
-Sidekiq.configure_server do |config|
-  config.redis = { namespace: Rails.configuration.active_job.queue_name_prefix }
-end
