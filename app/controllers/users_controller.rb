477b7bb1a91e2cecfd8abe9a98fecd3ac687fa3f
lucacug <luca.cugliari@unep-wcmc.org>
2024-03-12 16:21:38 +0000

    feat: display all the commitments on the admin dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def dashboard
-    commitments = current_user.commitments
+    commitments = current_user.admin? ? Commitment.published : current_user.commitments
     @commitments = commitments.to_json
     @has_commitments = commitments.present?
   end
